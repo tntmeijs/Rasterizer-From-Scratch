e136af9c8d521dc5b6893d74426795eac6cc28fb
Tahar Meijs <tntmeijs@gmail.com>
2019-01-31 21:45:19 +0100

    SFML hello world application
    [ADD] SFML code that renders a circle to the screen.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, char* argv[])
+int main()
 {
-    std::cout << "Hello, world!" << std::endl;
-    std::cin.get();
-    return 0;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
 }
