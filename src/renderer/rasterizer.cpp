d1926a47fd5eaaecaa520cf214f79bdbe692b97a
Tahar Meijs <tntmeijs@gmail.com>
2019-02-01 18:59:38 +0100

    Rasterization logic skeleton
    [ADD] Empty functions which will be used for rasterization.

--- src/renderer/rasterizer.cpp
+++ src/renderer/rasterizer.cpp
@@ -1,18 +1,83 @@
 #include "renderer/rasterizer.hpp"
 
+// Application
+#include "renderer/model.hpp"
+#include "renderer/structures.hpp"
+#include "settings.hpp"
+
 sr::Rasterizer::Rasterizer()
 	: m_window_width(0)
 	, m_window_height(0)
+	, m_pixel_data(nullptr)
+	, m_clear_color(0, 0, 0, 1)
 {
+}
+
+sr::Rasterizer::~Rasterizer()
+{
+	if (m_pixel_data)
+		delete[] m_pixel_data;
 }
 
 void sr::Rasterizer::Initialize(std::uint32_t window_width, std::uint32_t window_height)
 {
 	m_window_width = window_width;
 	m_window_height = window_height;
+
+	// Allocate memory to store the pixel data in
+	m_pixel_data = new std::uint8_t[window_width * window_height * static_cast<std::uint8_t>(settings::BYTES_PER_PIXEL)];
 }
 
 void sr::Rasterizer::AddModel(const std::shared_ptr<Model>& model) noexcept
 {
 	m_models.push_back(model);
+}
+
+void sr::Rasterizer::SetClearColor(const glm::vec4& clear_color)
+{
+	m_clear_color = clear_color;
+}
+
+void sr::Rasterizer::Render() noexcept
+{
+	for (size_t i = 0; i < m_models.size(); ++i)
+	{
+		if (auto ptr = m_models[i].lock())
+		{
+			// If no index data is available, rasterize the model without indices
+			if (0 == ptr->GetIndexDataSize())
+				RasterizeModelWithIndices(ptr);
+			else
+				RasterizeModelWithoutIndices(ptr);
+		}
+	}
+}
+
+void sr::Rasterizer::RasterizeModelWithIndices(const std::shared_ptr<Model>& model)
+{
+	const auto& vertex_data = model->GetVertexData();
+	size_t number_of_triangles = model->GetVertexDataSize() / sizeof(sr::Vertex);
+
+	if (auto ptr = vertex_data.lock())
+	{
+		// Since there are no indices, assume every three vertices make up a triangle
+		for (size_t i = 0; i < number_of_triangles; i += 3)
+		{
+			RasterizeTriangle(ptr.get()[i].position, ptr.get()[i + 1].position, ptr.get()[i + 2].position);
+		}
+	}
+}
+
+void sr::Rasterizer::RasterizeModelWithoutIndices(const std::shared_ptr<Model>& model)
+{
+	(void)model;
+	// #TODO: implement
+}
+
+void sr::Rasterizer::RasterizeTriangle(const glm::vec3& vertex_0, const glm::vec3& vertex_1, const glm::vec3& vertex_2)
+{
+	(void)vertex_0;
+	(void)vertex_1;
+	(void)vertex_2;
+	// #TODO: implement
 }
--- src/renderer/rasterizer.hpp
+++ src/renderer/rasterizer.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+// GLM
+#include <glm/glm.hpp>
+
 // C++ standard
 #include <cstdint>
 #include <memory>
@@ -14,16 +17,26 @@ namespace sr
 	{
 	public:
 		Rasterizer();
-		~Rasterizer() = default;
+		~Rasterizer();
 
 		void Initialize(std::uint32_t window_width, std::uint32_t window_height);
-
 		void AddModel(const std::shared_ptr<Model>& model) noexcept;
+		void SetClearColor(const glm::vec4& clear_color);
+		void Render() noexcept;
+
+	private:
+		void RasterizeModelWithIndices(const std::shared_ptr<Model>& model);
+		void RasterizeModelWithoutIndices(const std::shared_ptr<Model>& model);
+		void RasterizeTriangle(const glm::vec3& vertex_0, const glm::vec3& vertex_1, const glm::vec3& vertex_2);
 
 	private:
 		std::vector<std::weak_ptr<Model>> m_models;
 
 		std::uint32_t m_window_width;
 		std::uint32_t m_window_height;
+
+		std::uint8_t* m_pixel_data;
+
+		glm::vec4 m_clear_color;
 	};
 }
