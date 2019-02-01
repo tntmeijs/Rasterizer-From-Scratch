3f42d6ab5831d43db2636c6a8999faba7d0ba246
Tahar Meijs <tntmeijs@gmail.com>
2019-02-01 18:56:17 +0100

    Bytes per pixel
    [ADD] Bytes per pixel setting.

--- src/settings.hpp
+++ src/settings.hpp
@@ -5,8 +5,18 @@
 
 namespace settings
 {
+	namespace detail
+	{
+		enum class BytesPerPixel
+		{
+			RGBA = 4
+		};
+	}
+
 	const constexpr char* APPLICATION_NAME = "Software rasterizer | Tahar Meijs";
 	
 	const constexpr std::uint32_t WINDOW_WIDTH = 1280;
 	const constexpr std::uint32_t WINDOW_HEIGHT = 720;
+
+	const constexpr detail::BytesPerPixel BYTES_PER_PIXEL = detail::BytesPerPixel::RGBA;
 }
